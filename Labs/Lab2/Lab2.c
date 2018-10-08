d68ae642f4d7178ef456d110a90346d00bf1718e
Noah Prime <primen@wwu.edu>
2018-10-08 16:35:42 -0700

    Trying to sync up devices

---
+++ Labs/Lab2/Lab2.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFSIZE 4096
+
+char* itoa(int num, char* str, int base);
+
+int main(int argc, char *argv[]) {
+	int number, base;
+
+	if (argc != 3) {
+		printf("Usage: ./Lab2 <number> <base>\n");
+		return 1;
+	}
+
+	int res = sscanf(argv[1], "%d", &number);
+	if (!res || res == EOF) {
+		printf("Could not parse %s\n", argv[1]);
+		return 1;
+	}
+
+	res = sscanf(argv[2], "%d", &base);
+	if (!res || res == EOF) {
+		printf("Could not parse %s\n", argv[2]);
+		return 1;
+	} else if (!(2 <= base && base <= 36)) {
+		printf("Base must be between 2 and 36\n");
+		return 1;
+	}
+
+	char buf[BUFFSIZE];
+
+	if (itoa(number, buf, base) != buf) {
+		printf("Failed to convert %d to base %d\n", number, base);
+		return 1;
+	}
+
+	printf("Here is %d converted to base %d:\n%s\n", number, base, buf);
+
+	return 0;
+}
+
+char* itoa(int num, char* str, int base) {
+  if(num == 0){
+    *str = (char)0;
+    printf("\nString 0: %s\n",str);
+    return str;
+  }
+  else if(base <= 10){
+
+    int i = 0;
+    int j = 0;
+
+    if(num < 0){
+      str[i] = '-';
+      i++;
+      num = num*-1;
+      j++;
+    }
+
+    while(num != 0){
+      int rem = num%base + '0';
+      num = num/base;
+      str[i] = (char)rem;
+      i++;
+    }
+
+      i--;
+
+    while(i > j){
+      char dummy = str[i];
+      str[i] = str[j];
+      str[j] = dummy;
+      i--;
+      j++;
+    }
+
+    return str;
+  }
+  //////////////////////////////////////////////////////////////
+  //BASE GREATER THAN 10
+  ///////////////////////////////////////////////////////////////
+  else{
+    int i = 0;
+    int j = 0;
+
+    if(num < 0){
+      str[i] = '-';
+      i++;
+      num = num*-1;
+      j++;
+    }
+
+    while(num != 0){
+      int rem = num%base;
+      if(rem > 9){
+        rem = rem - 10 + 'a';
+      }
+      else{
+        rem = rem + '0';
+      }
+      num = num/base;
+      str[i] = (char)rem;
+      i++;
+    }
+
+      i--;
+
+    while(i > j){
+      char dummy = str[i];
+      str[i] = str[j];
+      str[j] = dummy;
+      i--;
+      j++;
+    }
+
+    return str;
+  }
+}
